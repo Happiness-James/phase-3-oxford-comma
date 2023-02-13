b62d6d1ca7da91e437af504f8c53cca85ba1cb1d
Happiness-James <gesarehjames@gmail.com>
2023-02-13 09:48:04 +0000

    assignment done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
+    if array.length == 1 
+        return array[0] 
+    end
 
+    last = array.pop
+
+    array.join(",") + " and #{last}" 
 end
+# p oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"])
